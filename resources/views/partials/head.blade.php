59f7cffd1ebc31a0bcd5dbe70cd32d9ceb6a61a0
陳姿穎 <roeyang1410@gmail.com>
2017-10-26 18:10:18 +0800

    定義共同區塊：在partials目錄裡新增head.blade.php

---
+++ resources/views/partials/head.blade.php
@@ -0,0 +1,4 @@
+<meta charset="UTF-8">
+Link rel="stylesheet" type="text/css" href="{{ asset('bower/bootstrap/dist/css/bootstrap.min.css')}}" />
+<script type="text/javascript" src="{{ asset('bower/jquery/dist/jquery.min.js')}}"></script>
+<script type="text/javascript" src="{{ asset('bower/bootstrap/dist/js/bootstrap.min.js')}}"></script>
