0462c44e367c348260d391cbb22fb80671e7607a
陳姿穎 <roeyang1410@gmail.com>
2017-10-26 10:29:44 +0800

    正規表達式限制參數:方法1

--- routes/web.php
+++ routes/web.php
@@ -29,3 +29,12 @@ Route::get('student/{student_no}/score/{subject}',function($student_no,$subject)
 Route::get('student/{student_no}/score/{subject}',function($student_no,$subject=null){
     return "學號：".$student_no."的".((is_null($subject))?"所有科目"：$subject)."成績";
 });
+
+//正規表達式限制參數-方法1
+Route::get('student/{student_no}',function($student_no){
+    return "學號："$student_no;
+}) ->where (['student_no'=>'s[0-9]{10}']);
+Route::get('student/{student_no}/score/{subject?}',function($student_no,$subject = null){
+    return "學號：".$student_no."的".((is_null($subject))?"所有科目":$subject)."成績";
+})->where(['student_no'=>'s[0-9]{10}','subject'=>'(chinese | english | math']);
+
