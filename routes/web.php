e0da98b2a97129b2bf081c03fe5458005c5f0c51
陳姿穎 <roeyang1410@gmail.com>
2017-10-26 10:31:28 +0800

    路由群組

--- routes/web.php
+++ routes/web.php
@@ -63,3 +63,15 @@ Route::group(['prefix'=>'student'],function() {
         }
     ])->where(['subject'=>'(chinese | english | math']);
 });
+
+//路由群組
+Route::pattern('student_no','s[0-9{10}');
+Route::group(['prefix'=>'student',function(){
+    Route::get('{student_no}',function($student_no){
+        return " 學號：".$student_no;
+    });
+    Route::get('{student_no}/score/{subject?}',function($student_no,$subject = null)
+    {
+        return "學號：" . $student_no . "的" . ((is_null($subject))? "所有科目": $subject)."成績";
+    })->where(['subject'=>'(chinese | english | math)']);
+}]);
