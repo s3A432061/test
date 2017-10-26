8451f0af16527fcf2db4f0b3e3a315b7096f121a
陳姿穎 <roeyang1410@gmail.com>
2017-10-26 18:36:26 +0800

    讓視圖顯示動態資料：將資料傳入View

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -3,15 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests;
+use App\Student;
+use View;
 
 class StudentController extends Controller
 {
     //
     public function getStudentData($student_no){
-        return "學號：".$student_no;
+        return View::make('score',[
+            'student_no'=>$student_no,
+            'subject'=>null
+        ]);
         }
 
         public function getStudentScore($student_no,$subject =null){
-        return "學號：".$student_no."的".((is_null($subject))?"所有科目":$subject)."成績：";
+        return View::make('score',[
+            'student_no'=>$student_no,
+            'subject'=>$subject
+        ]);
         }
 }
