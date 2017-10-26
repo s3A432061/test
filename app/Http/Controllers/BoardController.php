468456da6a25b19e4265bca0fc8a0f470bb5257c
陳姿穎 <roeyang1410@gmail.com>
2017-10-26 11:13:45 +0800

    在BoardController裡撰寫兩個方法

--- app/Http/Controllers/BoardController.php
+++ app/Http/Controllers/BoardController.php
@@ -7,4 +7,12 @@ use Illuminate\Http\Request;
 class BoardController extends Controller
 {
     //
+ public function getIndex(){
+     return "排行榜";
+ }
+    public function getName(){
+     return Route::currentRouteAcrion();
+    }
+
 }
+
