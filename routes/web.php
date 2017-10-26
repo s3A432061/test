83707670645b0ae8ad63fca212d4b4df3b100eb8
陳姿穎 <roeyang1410@gmail.com>
2017-10-26 10:25:59 +0800

    基礎路由參數：單個參數

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,7 @@
 Route::get('student/{student_no}', function ($student_no) {
     return "學號：".$student_no;
 });
+
+Route::get('student{student_no}/score',function ($student_no) {
+    return "學號：".$student_no."的所有成績";
+});
