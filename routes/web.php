fde40a8d014c87e1748259f8b6ba1e28bba3a1b9
陳姿穎 <roeyang1410@gmail.com>
2017-10-26 10:29:08 +0800

    選擇性路由參數

--- routes/web.php
+++ routes/web.php
@@ -25,3 +25,7 @@ Route::get('student/{student_no}/score/{subject}',function($student_no,$subject)
     return "學號：".$student_no."的".$subject."成績";
 });
 
+//選擇性路由參數
+Route::get('student/{student_no}/score/{subject}',function($student_no,$subject=null){
+    return "學號：".$student_no."的".((is_null($subject))?"所有科目"：$subject)."成績";
+});
